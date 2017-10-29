d013f99e4709aa804e1c9a8c93a2bf39096d79d7
grimm004 <grimm004@gmail.com>
2017-10-29 20:07:38 +0000

    Comparison

--- NumericalMethods/Program.cs
+++ NumericalMethods/Program.cs
@@ -19,17 +19,18 @@ namespace NumericalMethods
     {
         static void Main(string[] args)
         {
-            double x = 0.48119;
-            Console.WriteLine($"x = { x }");
-            double x0 = x - 0.00005;
-            double fx0 = F(x0);
-            Console.WriteLine($"x = { x0 } => F(x) = { fx0 }");
-            double x1 = x + 0.00005;
-            double fx1 = F(x1);
-            Console.WriteLine($"x = { x1 } => F(x) = { fx1 }");
+            //double x = 0.48119;
+            //Console.WriteLine($"x = { x }");
+            //double x0 = x - 0.000005;
+            //double fx0 = F(x0);
+            //Console.WriteLine($"x = { x0 } => F(x) = { fx0 }");
+            //double x1 = x + 0.000005;
+            //double fx1 = F(x1);
+            //Console.WriteLine($"x = { x1 } => F(x) = { fx1 }");
 
-            //int n = 20;
-            //Console.WriteLine($"iterations: { n } { NewtonRaphson.Calculate(n, 0, F, Fprime) }");
+            Console.WriteLine($"iterations: { 10 } { DecimalSearch.Calculate(10, 0, 1, F) }");
+            Console.WriteLine($"iterations: { 100 } { RearrangementMethod.Calculate(100, 0, G) }");
+            Console.WriteLine($"iterations: { 20 } { NewtonRaphson.Calculate(20, 0, F, Fprime) }");
 
             Console.ReadKey();
         }
@@ -44,6 +45,22 @@ namespace NumericalMethods
         {
             return -(3 * x * x) - (8 * x) - 2;
         }
+
+        static double G(double x)
+        {
+            return Math.Pow((-(x * x * x) - (2 * x) + 2) / 4, .5);
+        }
+
+        //static double F(double x)
+        //{
+        //    // negitive three x cubed plus two x squard plus half x minus one fifth
+        //    return -(3 * x * x * x) + (2 * x * x) + (.5 * x) - .2;
+        //}
+
+        //static double Fprime(double x)
+        //{
+        //    return -(9 * x * x) + (4 * x) + .5;
+        //}
 
         //static double F(double x)
         //{
